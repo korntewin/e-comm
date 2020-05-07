5a811a86c09ce9c9882d198702609d4587d94f9e
korntewin <korntewin.b@gmail.com>
2020-05-07 21:27:03 +0700

    finished styling directory menu and menu-item

--- src/components/directory-menu/directory-menu.components.jsx
+++ src/components/directory-menu/directory-menu.components.jsx
@@ -4,13 +4,14 @@ import {MenuItem} from '../menu-item/menu-item.components'
 
 export const DirectoryMenu = (props) => (
     <div className="directory-menu">
-        {props.sections.map(({title, id, imageUrl}) => {
+        {props.sections.map(({title, id, imageUrl, size}) => {
             return (
                 <MenuItem 
                 key = {id}
                 imageUrl = {imageUrl}
                 title = {title}
                 subtitle = {"Shop Now"}
+                size = {size}
                 ></MenuItem>
             )
         })}
--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import "./menu-item.styles.scss"
 
-export const MenuItem = ({title, subtitle, imageUrl}) => (
+export const MenuItem = ({title, subtitle, imageUrl, size}) => (
     <div 
-        style = {{
-            backgroundImage: `url(${imageUrl})`
-        }}
-        className = "menu-item">
+        className = {`menu-item ${size}`}>
+        <div 
+            style = {{
+                backgroundImage: `url(${imageUrl})`
+            }}
+            className="background-image" 
+        ></div>
         <div className="content">
             <h1 className="title"> {title.toUpperCase()} </h1>
             <span className="sub-title">{subtitle}</span>
