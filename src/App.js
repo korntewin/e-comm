592208e0a6bd2b5745ab2c3c7407d42f4e4ac7d9
korntewin <korntewin.b@gmail.com>
2020-05-10 18:10:36 +0700

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 import HomePage from "./components/pages/homepage/homepage.component"
+
+const JustAnotherPage = () => {
+  return (
+    <h1>Another Page</h1>
+  )
+}
 
 function App() {
   return (
 
     <div className = "App">
-      <HomePage />
+      <Switch>
+        <Route path = '/' component = {HomePage} />
+        <Route path='/another' component = {JustAnotherPage} />
+      </Switch>
     </div>
   );
 }
