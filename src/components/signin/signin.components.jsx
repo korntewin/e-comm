25585ebef57edb9aebe2fe0ba9ec3997b8c5d611
korntewin <korntewin.b@gmail.com>
2020-05-16 13:07:06 +0700

    add and finish styling form-input component

--- src/components/signin/signin.components.jsx
+++ src/components/signin/signin.components.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import './signin.styles.scss'
 
+import FormInput from '../form-input/form-input.components'
+
 class Signin extends Component {
     constructor(props) {
         super(props);
@@ -28,12 +30,22 @@ class Signin extends Component {
     render() {
         return (
             <div className="signin">
-                <h1>If you already have an id</h1>
+                <h1>Sign in with your email and password</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <label>Email</label>
-                    <input name='email' type='email' onChange={this.handleChange}/>
-                    <label>password</label>
-                    <input name='password' type='password' onChange={this.handleChange} />
+                    <FormInput 
+                        name='email' 
+                        type='email' 
+                        handleChange={this.handleChange}
+                        value = {this.state.email}
+                        label='email'    
+                    />
+                    <FormInput 
+                        name='password' 
+                        type='password' 
+                        handleChange={this.handleChange}
+                        value = {this.state.password}
+                        label='password'
+                    />
                     <button type='submit'>Sign In</button>
                 </form>
             </div>
